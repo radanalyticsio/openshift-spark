394e0c1d0eddc20128fd6904dafc659143ee509e
Matthew Farrellee <matt@redhat.com>
2015-03-17 15:47:47 -0400

    add note and fix default

--- master/start.sh
+++ master/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export SPARK_PUBLIC_DNS=${SPARK_MASTER_SERVICE_HOST:-$1}
+# start-master.sh uses SPARK_MATER_PORT and expects it to be an int, not a tcp:// url
+export SPARK_MASTER_PORT=${SPARK_MASTER_SERVICE_PORT:-7077}
 
 /usr/share/spark/sbin/start-master.sh
 
