0d6b765b567a706df59f1429b8eb51981cc447e3
Matthew Farrellee <matt@redhat.com>
2015-03-17 15:44:29 -0400

    watch logs instead of simply waiting

--- master/start.sh
+++ master/start.sh
@@ -3,6 +3,5 @@
 export SPARK_PUBLIC_DNS=${SPARK_MASTER_SERVICE_HOST:-$1}
 
 /usr/share/spark/sbin/start-master.sh
-PID=$(cat /tmp/spark-*.pid)
 
-while kill -0 $PID; do sleep 34; done
+tail -F /usr/share/spark/logs/*
--- worker/start.sh
+++ worker/start.sh
@@ -3,6 +3,5 @@
 export SPARK_PUBLIC_DNS=${SPARK_MASTER_SERVICE_HOST:-$1}
 
 /usr/share/spark/sbin/start-slave.sh 1 spark://$SPARK_PUBLIC_DNS:7077
-PID=$(cat /tmp/spark-*.pid)
 
-while kill -0 $PID; do sleep 34; done
+tail -F /usr/share/spark/logs/*
