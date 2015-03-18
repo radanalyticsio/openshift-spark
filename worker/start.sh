ae08a566465eea9f13bff865e3ab04b6dbd46f94
Matthew Farrellee <matt@redhat.com>
2015-03-18 08:52:46 -0400

    simplify name resolution setup

--- worker/start.sh
+++ worker/start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-MASTER_IP=${SPARK_MASTER_SERVICE_HOST:-$1}
-echo "MASTER_IP=$MASTER_IP"
-
 # name resolution for spark-master
-echo "$MASTER_IP spark-master" >> /etc/hosts
+echo "${SPARK_MASTER_SERVICE_HOST:-$1} spark-master" >> /etc/hosts
 
 /usr/share/spark/sbin/start-slave.sh 1 spark://spark-master:7077
 
