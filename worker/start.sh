5647dbac2ec7d87f3a3caca1b9793b9c43a76940
Matthew Farrellee <matt@redhat.com>
2015-03-18 08:53:29 -0400

    make worker report its ip instead of hostname, enables driver connections

--- worker/start.sh
+++ worker/start.sh
@@ -3,6 +3,9 @@
 # name resolution for spark-master
 echo "${SPARK_MASTER_SERVICE_HOST:-$1} spark-master" >> /etc/hosts
 
+# because the hostname only resolves locally
+export SPARK_LOCAL_HOSTNAME=$(hostname -i)
+
 /usr/share/spark/sbin/start-slave.sh 1 spark://spark-master:7077
 
 tail -F /usr/share/spark/logs/*
