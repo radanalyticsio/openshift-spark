ba73512364539cb7bc14b38b2e99efe834fefd78
Matthew Farrellee <matt@redhat.com>
2015-07-27 14:03:10 -0400

    start-slave.sh no longer needs worker id as an argument

--- worker/start.sh
+++ worker/start.sh
@@ -6,7 +6,7 @@ echo "${SPARK_MASTER_SERVICE_HOST:-$1} spark-master" >> /etc/hosts
 # because the hostname only resolves locally
 export SPARK_LOCAL_HOSTNAME=$(hostname -i)
 
-/usr/share/spark/sbin/start-slave.sh 1 spark://spark-master:7077
+/usr/share/spark/sbin/start-slave.sh spark://spark-master:7077
 
 # TODO: detect slave exit
 tail -F /usr/share/spark/logs/*
