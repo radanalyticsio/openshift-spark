6497e161a9fb5444a15897b95d79c406dbb36b00
Matthew Farrellee <matt@redhat.com>
2015-12-04 10:50:32 -0500

    use nss_wrapper to modify hosts db

--- master/start.sh
+++ master/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+cat /etc/hosts > /tmp/hosts
+echo "$(hostname -i) spark-master" >> /tmp/hosts
+export NSS_WRAPPER_HOSTS=/tmp/hosts
+
 # start-master.sh uses SPARK_MATER_PORT and expects it to be an int, not a tcp:// url
 export SPARK_MASTER_PORT=${SPARK_MASTER_SERVICE_PORT:-7077}
 
