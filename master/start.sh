16eb38d5bb955ea96f34ed2c855abefdb109d911
Matthew Farrellee <matt@redhat.com>
2015-12-04 17:42:20 -0500

    unset SPARK_MASTER_PORT and SPARK_MASTER_WEBUI_PORT, they are invalidly set and override command line arguments

--- master/start.sh
+++ master/start.sh
@@ -4,8 +4,8 @@
 
 echo "$(hostname -i) spark-master" >> /tmp/hosts
 
-# start-master.sh uses SPARK_MATER_PORT and expects it to be an int, not a tcp:// url
-export SPARK_MASTER_PORT=${SPARK_MASTER_SERVICE_PORT:-7077}
+unset SPARK_MASTER_PORT
+unset SPARK_MASTER_WEBUI_PORT
 
 # run the spark master directly (instead of sbin/start-master.sh) to
 # link master and container lifecycle
