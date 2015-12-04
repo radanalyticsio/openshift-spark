c22c3fee9b07c5d79cd2a4a2c8623c0d8280dd3a
Matthew Farrellee <matt@redhat.com>
2015-12-04 12:40:06 -0500

    link container and spark master lifecycles

--- master/start.sh
+++ master/start.sh
@@ -7,7 +7,7 @@ echo "$(hostname -i) spark-master" >> /tmp/hosts
 # start-master.sh uses SPARK_MATER_PORT and expects it to be an int, not a tcp:// url
 export SPARK_MASTER_PORT=${SPARK_MASTER_SERVICE_PORT:-7077}
 
-/usr/share/spark/sbin/start-master.sh
-
-# TODO: detect master exit
-tail -F /tmp/logs/*
+# run the spark master directly (instead of sbin/start-master.sh) to
+# link master and container lifecycle
+/usr/share/spark/bin/spark-class \
+  org.apache.spark.deploy.master.Master --ip spark-master --port 7077 --webui-port 8080
