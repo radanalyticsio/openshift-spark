2ed7775b528b2aaaa8fa5f76ea1a0007d9b8ad14
Matthew Farrellee <matt@redhat.com>
2015-12-28 16:39:50 -0500

    stop faking spark-master name
    previously the master would use /tmp/hosts to pretend it was called
    spark-master, and workers would likewise to talk to the master.
    
    instead, the name of the master service is passed to the master's
    start.sh, and the master pretends to be the service. the service was
    already passed to the worker's start.sh and it still is, except now
    the worker just tries to connect to the service.

--- master/start.sh
+++ master/start.sh
@@ -2,11 +2,11 @@
 
 . /common.sh
 
-echo "$(hostname -i) spark-master" >> /tmp/hosts
+echo "$(hostname -i) $1" >> /tmp/hosts
 
 unset SPARK_MASTER_PORT
 unset SPARK_MASTER_WEBUI_PORT
 
 # run the spark master directly (instead of sbin/start-master.sh) to
 # link master and container lifecycle
-spark-class org.apache.spark.deploy.master.Master --ip spark-master --port 7077 --webui-port 8080
+spark-class org.apache.spark.deploy.master.Master --ip $1 --port 7077 --webui-port 8080
--- worker/start.sh
+++ worker/start.sh
@@ -2,12 +2,7 @@
 
 . /common.sh
 
-# replace dashes with underscores
-name=${1//-/_}
-# write name resolution for spark-master
-echo "$(eval echo \$${name^^}_SERVICE_HOST) spark-master" >> /tmp/hosts
-
 # because the hostname only resolves locally
 export SPARK_LOCAL_HOSTNAME=$(hostname -i)
 
-spark-class org.apache.spark.deploy.worker.Worker spark://spark-master:7077
+spark-class org.apache.spark.deploy.worker.Worker spark://$1:7077
