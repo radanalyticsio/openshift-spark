9cb866a85cc9dd607140c99d52fa890d440b8d01
Matthew Farrellee <matt@redhat.com>
2015-12-28 20:44:32 -0500

    add commentary for master's start.sh

--- master/start.sh
+++ master/start.sh
@@ -2,8 +2,16 @@
 
 . /common.sh
 
+# the name the master calls itself needs to be the same as what the
+# workers call it. the workers use the spark-master-service name, and
+# thus so shall the master.
 echo "$(hostname -i) $1" >> /tmp/hosts
 
+# unset these env variables that are used by the Master, because they
+# may be set and if so they're likely to be set to kubernetes
+# specified values instead of values Master can understand. for
+# instance, SPARK_MASTER_PORT=tcp://172.30.74.44:7077 instead of just
+# 7077.
 unset SPARK_MASTER_PORT
 unset SPARK_MASTER_WEBUI_PORT
 
