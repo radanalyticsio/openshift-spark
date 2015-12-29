d95aef8b0eaed8208a4a3ade9ae0c3bd9b4c4968
Matthew Farrellee <matt@redhat.com>
2015-12-29 07:20:46 -0500

    Revert "add proper opt parsing to master"
    This reverts commit a257989670484bd56cfe2b341aea983dcd3b6dc4.

--- master/start.sh
+++ master/start.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 
-while getopts "n:" opt; do
-  case $opt in
-    n)
-      name=$OPTARG
-      ;;
-  esac
-done
-
-if [ -z "$name" ]; then
-  echo "No name provided, -n required, e.g. -n spark-master.local" >&2
-  exit 1
-fi
-
-echo "Starting master, using name: $name"
-
 . /common.sh
 
 # the name the master calls itself needs to be the same as what the
 # workers call it. the workers use the spark-master-service name, and
 # thus so shall the master.
-echo "$(hostname -i) $name" >> /tmp/hosts
+echo "$(hostname -i) $1" >> /tmp/hosts
 
 # unset these env variables that are used by the Master, because they
 # may be set and if so they're likely to be set to kubernetes
@@ -32,4 +17,4 @@ unset SPARK_MASTER_WEBUI_PORT
 
 # run the spark master directly (instead of sbin/start-master.sh) to
 # link master and container lifecycle
-spark-class org.apache.spark.deploy.master.Master --ip $name --port 7077 --webui-port 8080
+spark-class org.apache.spark.deploy.master.Master --ip $1 --port 7077 --webui-port 8080
