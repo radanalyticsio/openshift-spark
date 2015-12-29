33a6ca0d654383c7c367b4cbb2ecd05698854eb1
Matthew Farrellee <matt@redhat.com>
2015-12-28 20:38:40 -0500

    add arg parsing to worker start.sh
    the worker's start.sh now requires -m <master>, where <master> is
    likely spark://spark-master-ABCD:7077

--- worker/start.sh
+++ worker/start.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+while getopts "m:" opt; do
+  case $opt in
+    m)
+      master=$OPTARG
+      ;;
+  esac
+done
+
+if [ -z "$master" ]; then
+  echo "No master provided, -m required, e.g. -m spark-master.local" >&2
+  exit 1
+fi
+
+echo "Starting worker, will connect to: $master"
+
 . /common.sh
 
 # because the hostname only resolves locally
 export SPARK_LOCAL_HOSTNAME=$(hostname -i)
 
-spark-class org.apache.spark.deploy.worker.Worker spark://$1:7077
+spark-class org.apache.spark.deploy.worker.Worker $master
