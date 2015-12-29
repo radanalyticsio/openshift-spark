bcb33933e22f3632d67093f97988f3dce538105f
Matthew Farrellee <matt@redhat.com>
2015-12-29 07:55:20 -0500

    pass SPARK_MASTER_PORT and SPARK_MASTER_WEBUI_PORT in from the template

--- master/start.sh
+++ master/start.sh
@@ -8,14 +8,6 @@
 echo "$(hostname -i) $1" >> /tmp/hosts
 export SPARK_MASTER_HOST=$1
 
-# unset these env variables that are used by the Master, because they
-# may be set and if so they're likely to be set to kubernetes
-# specified values instead of values Master can understand. for
-# instance, SPARK_MASTER_PORT=tcp://172.30.74.44:7077 instead of just
-# 7077.
-unset SPARK_MASTER_PORT
-unset SPARK_MASTER_WEBUI_PORT
-
 # run the spark master directly (instead of sbin/start-master.sh) to
 # link master and container lifecycle
-spark-class org.apache.spark.deploy.master.Master
+exec spark-class org.apache.spark.deploy.master.Master
