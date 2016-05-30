d944997368d7d12b407bc15b8c53dfb06f9c21cf
Matthew Farrellee <matt@redhat.com>
2016-05-30 07:19:06 -0400

    use SPARK_USER instead of NSS_WRAPPER_{PASSWD,GROUP}

--- common.sh
+++ common.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 # spark likes to be able to lookup a username for the running UID, if
-# no name is present fake it. it may also be possible to use the
-# SPARK_USER environment variable.
+# no name is present fake it.
 if [[ ! $(getent passwd $(id -u)) ]]; then
-  if [[ ! -e /tmp/passwd ]]; then
-    cat /etc/passwd > /tmp/passwd
-    echo "$(id -u):x:$(id -u):$(id -g):dynamic uid:/opt/spark:/bin/false" >> /tmp/passwd
-  fi
+  export SPARK_USER=$(id -u)
 fi
-
-export NSS_WRAPPER_PASSWD=/tmp/passwd
-# NSS_WRAPPER_GROUP must be set for NSS_WRAPPER_PASSWD to be used
-export NSS_WRAPPER_GROUP=/etc/group
