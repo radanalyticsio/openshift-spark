00c94a5ba2723ae319da12ec62b12f55b77e6daa
Trevor McKay <tmckay@redhat.com>
2017-01-19 12:13:20 -0500

    Copy local spark configuration to $SPARK_HOME/conf
    Rather than have the pod creator set the SPARK_CONF_DIR environment
    variable to /etc/oshinko-spark-configs to modify the spark configuration,
    this change simply copies the contents of /etc/oshinko-spark-configs
    if present to $SPARK_HOME/conf. This gives us a simple overwrite semantic
    so that you only need to specify the files being changed as opposed
    to also including copies of files not being changed.

--- scripts/spark/added/launch.sh
+++ scripts/spark/added/launch.sh
@@ -11,6 +11,21 @@ export NSS_WRAPPER_GROUP=/etc/group
 
 export LD_PRELOAD=libnss_wrapper.so
 
+# If the /etc/oshinko-spark-configs dir is non-empty,
+# copy the contents to $SPARK_HOME/conf
+USER_SPARK_CONF=/etc/oshinko-spark-configs
+ls -1 $USER_SPARK_CONF &> /dev/null
+if [ $? -eq 0 ]; then
+    sparkconfs=$(ls -1 /etc/oshinko-spark-configs | wc -l)
+    if [ "$sparkconfs" -ne "0" ]; then
+        echo "Copying $USER_SPARK_CONF/* to $SPARK_HOME/conf"
+        ls -1 /etc/oshinko-spark-configs
+        cp $USER_SPARK_CONF/* $SPARK_HOME/conf
+    fi
+else
+    echo "/etc/oshinko-spark-configs does not exist, using default spark config"
+fi
+
 # If SPARK_MASTER_ADDRESS env varaible is not provided, start master,
 # otherwise start worker and connect to SPARK_MASTER_ADDRESS
 if [ -z ${SPARK_MASTER_ADDRESS+_} ]; then
