25705a7b8718fbff87fc78d79e6f9a90ee583127
Matthew Farrellee <matt@redhat.com>
2017-01-24 12:40:02 -0500

    move nss_wrapper setup code to an entrypoint
    the goal is to allow users to shell into the cluster pods, or start a
    container manually, without having to manually setup nss_wrapper in
    their environment.
    
    everyone stumbles on getting the nss_wrapper env setup the first
    time. the resulting error is about failure to login from within the
    hadoop code, which is confusing. everyone then struggles with the
    manual nature of setting up the env after they overcome the strange
    login error.

--- scripts/spark/added/launch.sh
+++ scripts/spark/added/launch.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# spark likes to be able to lookup a username for the running UID, if
-# no name is present fake it.
-cat /etc/passwd > /tmp/passwd
-echo "$(id -u):x:$(id -u):$(id -g):dynamic uid:$SPARK_HOME:/bin/false" >> /tmp/passwd
-
-export NSS_WRAPPER_PASSWD=/tmp/passwd
-# NSS_WRAPPER_GROUP must be set for NSS_WRAPPER_PASSWD to be used
-export NSS_WRAPPER_GROUP=/etc/group
-
-export LD_PRELOAD=libnss_wrapper.so
-
 # If the UPDATE_SPARK_CONF_DIR dir is non-empty,
 # copy the contents to $SPARK_HOME/conf
 if [ -d "$UPDATE_SPARK_CONF_DIR" ]; then
