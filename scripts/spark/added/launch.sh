160bc418c51b8009734b31a25c4499e926b21dcd
Trevor McKay <tmckay@redhat.com>
2018-01-03 11:28:23 -0500

    Add a check and timeout in signal handler
    If the spark subprocess does not stop within 10 seconds
    after a TERM signal is sent, send a unmaskable termination.
    This is the same approach used in radanalyticsio s2i driver
    images.

--- scripts/spark/added/launch.sh
+++ scripts/spark/added/launch.sh
@@ -11,11 +11,28 @@ function check_reverse_proxy {
 
 function handle_term {
     echo Received a termination signal
+
     # If we've saved a PID for a subprocess, kill that first before
     # trying to delete the cluster
+    local cnt
+    local killed=1
     if [ -n "$PID" ]; then
         echo "Stopping subprocess $PID"
         kill -TERM $PID
+        for cnt in {1..10}
+        do
+            kill -0 $PID >/dev/null 2>&1
+            if [ "$?" -ne 0 ]; then
+                killed=0
+                break
+            else
+                sleep 1
+            fi
+        done
+        if [ "$killed" -ne 0 ]; then
+            echo Process is still running 10 seconds after TERM, sending KILL
+            kill -9 $PID
+        fi
         wait $PID
         echo "Subprocess stopped"
     fi
