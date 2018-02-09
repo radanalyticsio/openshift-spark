4dc54eaac0e72a7fc91ccde0f6de496b8ae4b7eb
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2018-02-09 15:05:13 +0000

    delete template after test

--- test/cmd/config-changes.sh
+++ test/cmd/config-changes.sh
@@ -21,3 +21,5 @@ os::cmd::try_until_success 'oc delete dc/master'
 
 #check the pods have been deleted using a label
 os::cmd::try_until_text 'oc get pods' 'No resources found.' $((15*second))
+
+os::cmd::expect_success 'oc delete template spark'
