ab88e364dbf79efb9f069dd367526ace5a3f6bd6
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2018-02-09 15:45:34 +0000

    all changes apart from metrics

--- test/cmd/config-changes.sh
+++ test/cmd/config-changes.sh
@@ -6,14 +6,19 @@ RESOURCE_DIR="$(dirname "${BASH_SOURCE}")/../resources"
 
 os::test::junit::declare_suite_start "cmd/create"
 
-os::cmd::expect_success 'oc create configmap test-config --from-file=$RESOURCE_DIR'
+os::cmd::expect_success 'oc create configmap test-config --from-file=$RESOURCE_DIR/config'
 
-os::cmd::expect_success 'oc create -f $RESOURCE_DIR/test-template.yaml'
-
-os::cmd::expect_success 'oc new-app --template=spark -p MASTER_NAME=master -p WORKER_NAME=worker -p SPARK_IMAGE="$OPENSHIFT_SPARK_TEST_IMAGE"'
+os::cmd::expect_success 'oc new-app --file=$RESOURCE_DIR/test-template.yaml -p MASTER_NAME=master -p WORKER_NAME=worker -p SPARK_IMAGE="$OPENSHIFT_SPARK_TEST_IMAGE"'
 
 #check the logging has changed so you know a new config is used
 os::cmd::try_until_text 'oc logs dc/master' 'DEBUG'
+
+os::cmd::try_until_text 'oc logs dc/worker' 'DEBUG'
+
+os::cmd::try_until_text 'oc logs dc/master' 'Copying from /etc/config to /opt/spark/conf'
+
+os::cmd::try_until_text 'oc logs dc/worker''Copying from /etc/config to /opt/spark/conf'
+
 #test deletion
 os::cmd::try_until_success 'oc delete dc/worker'
 
