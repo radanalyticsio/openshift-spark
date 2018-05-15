2e55abdf7b4326acc0978b2213ad4654039cd66a
Michael McCune <msm@redhat.com>
2018-05-15 18:17:56 -0400

    add initial python36 version stuff
    * add concreate overrides for default and py36 versions
    * move numpy installation to default override
    * add a python36 module
    * add an entry point variable
    * update documentation
    * remove unused script build-spark-push.sh
    * add a test line for py36 images

--- README.md
+++ README.md
@@ -4,10 +4,38 @@
 
 # Apache Spark images for OpenShift
 
-# Build
+This repository contains several files for building
+[Apache Spark](https://spark.apache.org) focused container images, targeted
+for usage on [OpenShift Origin](https://openshift.org).
+
+By default, it will build the following images into your local Docker
+registry:
+
+* `openshift-spark`, Apache Spark, Python 2.7
+* `openshift-spark-py36`, Apache Spark, Python 3.6
+
+For Spark versions, please see the `image.yaml` file.
+
+# Instructions
+
+## Build
+
+Create all images and save them in the local Docker registry.
 
     make
 
-# Push
+## Push
 
-    make push SPARK_IMAGE=[REGISTRY_HOST/][USERNAME]
+Tag and push the images to the designated reference.
+
+    make push SPARK_IMAGE=[REGISTRY_HOST[:REGISTRY_PORT]/]NAME[:TAG]
+
+# Customization
+
+There are several ways to customize the construction and build process. This
+project uses the [GNU Make tool](https://www.gnu.org/software/make/) for
+the build workflow, see the `Makefile` for more information. For container
+specification and construction, the
+[Container image creation tool `concreate`](https://github.com/cekit/cekit) is
+used as the primary point of investigation, see the `image.yaml` file for
+more information.
---
+++ openshift-spark-build-py36/modules/spark/added/launch.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function check_reverse_proxy {
+    grep -e "^spark\.ui\.reverseProxy" $SPARK_HOME/conf/spark-defaults.conf &> /dev/null
+    if [ "$?" -ne 0 ]; then
+        echo "Appending default reverse proxy config to spark-defaults.conf"
+        echo "spark.ui.reverseProxy              true" >> $SPARK_HOME/conf/spark-defaults.conf
+        echo "spark.ui.reverseProxyUrl           /" >> $SPARK_HOME/conf/spark-defaults.conf
+    fi
+}
+
+# If the UPDATE_SPARK_CONF_DIR dir is non-empty,
+# copy the contents to $SPARK_HOME/conf
+if [ -d "$UPDATE_SPARK_CONF_DIR" ]; then
+    sparkconfs=$(ls -1 $UPDATE_SPARK_CONF_DIR | wc -l)
+    if [ "$sparkconfs" -ne "0" ]; then
+        echo "Copying from $UPDATE_SPARK_CONF_DIR to $SPARK_HOME/conf"
+        ls -1 $UPDATE_SPARK_CONF_DIR
+        cp $UPDATE_SPARK_CONF_DIR/* $SPARK_HOME/conf
+    fi
+elif [ -n "$UPDATE_SPARK_CONF_DIR" ]; then
+    echo "Directory $UPDATE_SPARK_CONF_DIR does not exist, using default spark config"
+fi
+
+check_reverse_proxy
+
+if [ -z ${SPARK_METRICS_ON+_} ]; then
+    JAVA_AGENT=
+    metrics=""
+elif [ ${SPARK_METRICS_ON} == "prometheus" ]; then
+    JAVA_AGENT=" -javaagent:$SPARK_HOME/agent-bond.jar=$SPARK_HOME/conf/agent.properties"
+    metrics=" with prometheus metrics enabled"
+else
+    JAVA_AGENT=" -javaagent:$SPARK_HOME/jolokia-jvm-1.3.6-agent.jar=port=7777,host=0.0.0.0"
+    metrics=" with jolokia metrics enabled (deprecated, set SPARK_METRICS_ON to 'prometheus')"
+fi
+
+if [ -z ${SPARK_MASTER_ADDRESS+_} ]; then
+    echo "Starting master$metrics"
+    exec $SPARK_HOME/bin/spark-class$JAVA_AGENT org.apache.spark.deploy.master.Master
+else
+    echo "Starting worker$metrics, will connect to: $SPARK_MASTER_ADDRESS"
+    while true; do
+        echo "Waiting for spark master to be available ..."
+        curl --connect-timeout 1 -s -X GET $SPARK_MASTER_UI_ADDRESS > /dev/null
+        if [ $? -eq 0 ]; then
+            break
+        fi
+        sleep 1
+    done
+    exec $SPARK_HOME/bin/spark-class$JAVA_AGENT org.apache.spark.deploy.worker.Worker $SPARK_MASTER_ADDRESS
+fi
