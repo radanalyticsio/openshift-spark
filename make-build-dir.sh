02340d4baea4fe42117d69b935d3b6c478d3de50
Trevor McKay <tmckay@redhat.com>
2018-09-10 15:02:38 -0400

    Include incomplete image directories

--- make-build-dir.sh
+++ make-build-dir.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 # Regenerate the build directory based on image.*.yaml
+make clean-target
 make clean-context
+make -f Makefile.inc clean-context
+
 make context
+make -f Makefile.inc context
+
 make zero-tarballs
+make -f Makefile.inc zero-tarballs
 
 # Add any changes for a commit
 git add openshift-spark-build
 git add openshift-spark-build-py36
+git add openshift-spark-build-inc
+git add openshift-spark-build-inc-py36
