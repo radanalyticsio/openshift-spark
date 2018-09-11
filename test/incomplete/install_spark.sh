c0ae82a3a160443917864afaa746c461d698623d
Trevor McKay <tmckay@redhat.com>
2018-09-11 15:23:49 -0400

    Add build without md5 and with bad md5 to spark install test

--- test/incomplete/install_spark.sh
+++ test/incomplete/install_spark.sh
@@ -10,28 +10,54 @@ RESOURCE_DIR=$TOP_DIR/test/resources
 
 os::test::junit::declare_suite_start "install_spark"
 
+function build_md5 {
+    os::cmd::expect_success 'oc new-build --name=spark --docker-image="$SPARK_IMAGE" --binary'
+    poll_binary_build spark "$RESOURCE_DIR"/spark-inputs
+
+    os::cmd::expect_success_and_text 'oc log buildconfig/spark' 'Attempting to install Spark'
+    os::cmd::expect_success_and_text 'oc log buildconfig/spark' 'Push successful'
+
+    if [ "$#" -ne 1 ] || [ "$1" != "true" ]; then
+        # Now we should have an imagestream named spark
+        SPARK_PULL=$(oc get is spark --template='{{index .status "dockerImageRepository"}}')
+        os::cmd::expect_success 'oc new-app --file=$RESOURCE_DIR/test-template.yaml -p MASTER_NAME=master -p WORKER_NAME=worker -p SPARK_IMAGE=$SPARK_PULL'
+
+        get_cluster_pod master
+        os::cmd::try_until_text 'oc logs $POD' 'Starting.*master'
+
+        get_cluster_pod worker
+        os::cmd::try_until_text 'oc logs $POD' 'Starting.*worker'
+    fi
+    os::cmd::expect_success 'oc delete buildconfig spark'
+}
+
+function build_bad_md5 {
+    os::cmd::expect_success 'oc new-build --name=spark --docker-image="$SPARK_IMAGE" --binary'
+    poll_binary_build spark "$RESOURCE_DIR"/spark-inputs true
+
+    os::cmd::expect_success_and_text 'oc log buildconfig/spark' 'md5sum did not match'
+    os::cmd::expect_success 'oc delete buildconfig spark'
+}
+
 # Handles registries, etc, and sets SPARK_IMAGE to the right value
 make_image
 make_configmap
 
-os::cmd::expect_success 'oc new-build --name=spark --docker-image="$SPARK_IMAGE" --binary'
+echo "++ build with md5"
+#build_md5
 
-poll_binary_build spark "$RESOURCE_DIR"/spark-inputs
+echo "++ build without md5"
+echo $RESOURCE_DIR
+find $RESOURCE_DIR -name "*.md5"
+md5=$(find $RESOURCE_DIR/spark-inputs -name "*.md5")
+rm $md5
+skip_app=true
+build_md5 $skip_app
 
-os::cmd::expect_success_and_text 'oc log buildconfig/spark' 'Attempting to install Spark'
-os::cmd::expect_success_and_text 'oc log buildconfig/spark' 'Push successful'
-
-# Now we should have an imagestream named spark
-SPARK_PULL=$(oc get is spark --template='{{index .status "dockerImageRepository"}}')
-os::cmd::expect_success 'oc new-app --file=$RESOURCE_DIR/test-template.yaml -p MASTER_NAME=master -p WORKER_NAME=worker -p SPARK_IMAGE=$SPARK_PULL'
-
-get_cluster_pod master
-os::cmd::try_until_text 'oc logs $POD' 'Starting.*master'
-
-get_cluster_pod worker
-os::cmd::try_until_text 'oc logs $POD' 'Starting.*worker'
-
-oc delete buildconfig spark
+echo "++ build with bad md5"
+mv $RESOURCE_DIR/spark-inputs/$(basename $md5 .md5).bad $md5
+build_bad_md5
+rm $md5
 
 cleanup_app
 
--- test/sparkinputs.sh
+++ test/sparkinputs.sh
@@ -9,6 +9,7 @@ filename=$(basename $fullname)
 version=$(echo $filename | cut -d '-' -f2)
 mkdir -p $TOP_DIR/test/resources/spark-inputs
 pushd $TOP_DIR/test/resources/spark-inputs
-wget https://archive.apache.org/dist/spark/spark-$version/spark-$version-bin-hadoop2.7.tgz
+#wget https://archive.apache.org/dist/spark/spark-$version/spark-$version-bin-hadoop2.7.tgz
 wget https://archive.apache.org/dist/spark/spark-$version/spark-$version-bin-hadoop2.7.tgz.md5
+echo "spark-$version-bin-hadoop2.7.tgz: FF FF FF FF FF FF CA FE  BE EF CA FE BE EF CA FE" > spark-$version-bin-hadoop2.7.tgz.bad
 popd
