d1652cfb0f96c6ffeeb50e74aa10c8e9edcd1245
Trevor McKay <tmckay@redhat.com>
2018-09-12 16:07:50 -0400

    Add additional incomplete image build tests
    * test building with spark urls as env vars
    * test a binary build with an image containing spark (should fail)
    * test a build using a tarball with a bad spark-submit
    * test copy/nocopy of configuration files
    * check for output messages
    
    +echo "++ build_env_var"
    +build_env_var
    +
    +echo "++ already_installed"
    +already_installed
    +
    +echo "++ bad_submit"
    +bad_submit
    +
    +echo "++ copy_nocopy"
    +copy_nocopy

--- test/sparkinputs.sh
+++ test/sparkinputs.sh
@@ -11,8 +11,12 @@ filename=$(basename $fullname)
 version=$(echo $filename | cut -d '-' -f2)
 mkdir -p $TOP_DIR/test/resources/spark-inputs
 pushd $TOP_DIR/test/resources/spark-inputs
-wget https://archive.apache.org/dist/spark/spark-$version/spark-$version-bin-hadoop2.7.tgz
-wget https://archive.apache.org/dist/spark/spark-$version/spark-$version-bin-hadoop2.7.tgz.md5
+if ! [ -f "spark-$version-bin-hadoop2.7.tgz" ]; then
+    wget https://archive.apache.org/dist/spark/spark-$version/spark-$version-bin-hadoop2.7.tgz
+fi
+if ! [ -f "spark-$version-bin-hadoop2.7.tgz.md5" ]; then
+    wget https://archive.apache.org/dist/spark/spark-$version/spark-$version-bin-hadoop2.7.tgz.md5
+fi
 echo "spark-$version-bin-hadoop2.7.tgz: FF FF FF FF FF FF CA FE  BE EF CA FE BE EF CA FE" > spark-$version-bin-hadoop2.7.tgz.bad
 popd
 
@@ -21,6 +25,32 @@ mkdir -p $TOP_DIR/test/resources/spark-inputs-no-submit
 pushd $TOP_DIR/test/resources/spark-inputs-no-submit
 mkdir spark-$version-bin-hadoop2.7
 touch spark-$version-bin-hadoop2.7/foo
+tar -czf spark-$version-bin-hadoop2.7.tgz spark-$version-bin-hadoop2.7
+rm -rf spark-$version-bin-hadoop2.7
+popd
+
+# Make a fake tarball with a spark-submit that returns an error
+mkdir -p $TOP_DIR/test/resources/spark-inputs-bad-submit
+pushd $TOP_DIR/test/resources/spark-inputs-bad-submit
+mkdir -p spark-$version-bin-hadoop2.7/bin
+echo "#!/bin/bash" > spark-$version-bin-hadoop2.7/bin/spark-submit
+echo "exit 1" >> spark-$version-bin-hadoop2.7/bin/spark-submit
+chmod +x spark-$version-bin-hadoop2.7/bin/spark-submit
+tar -czf spark-$version-bin-hadoop2.7.tgz spark-$version-bin-hadoop2.7
+rm -rf spark-$version-bin-hadoop2.7
+popd
+
+# Make a fake tarball with a spark-submit that returns success
+# Also include some config files so we can test copy-if-not-overwrite
+mkdir -p $TOP_DIR/test/resources/spark-inputs-with-conf
+pushd $TOP_DIR/test/resources/spark-inputs-with-conf
+mkdir -p spark-$version-bin-hadoop2.7/bin
+echo "#!/bin/bash" > spark-$version-bin-hadoop2.7/bin/spark-submit
+echo "exit 0" >> spark-$version-bin-hadoop2.7/bin/spark-submit
+chmod +x spark-$version-bin-hadoop2.7/bin/spark-submit
+mkdir -p spark-$version-bin-hadoop2.7/conf
+touch spark-$version-bin-hadoop2.7/conf/spark-defaults.conf
+touch spark-$version-bin-hadoop2.7/conf/log4j.properties
 tar -czf spark-$version-bin-hadoop2.7.tgz spark-$version-bin-hadoop2.7
 rm -rf spark-$version-bin-hadoop2.7
 popd
